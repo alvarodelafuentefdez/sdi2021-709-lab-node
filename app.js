2b385d18bdfac9d9e3355e2f054d69eaa35183c2
uo251950 <uo251950@uniovi.es>
2021-04-07 20:33:36 +0200

    SDI-2021-709-8.4-Registro de usuario, autenticación y uso de sesión.

--- app.js
+++ app.js
@@ -16,9 +16,16 @@ app.use(express.static('public'));
 let gestorBD = require("./modules/gestorBD.js");
 gestorBD.init(app, mongo);
 
+let expressSession = require('express-session');
+app.use(expressSession({secret: 'abcdefg', resave: true, saveUninitialized: true}));
+
+let crypto = require('crypto');
+
 // Variables
 app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.lrbos.mongodb.net:27017,tiendamusica-shard-00-01.lrbos.mongodb.net:27017,tiendamusica-shard-00-02.lrbos.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-sv0k89-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
