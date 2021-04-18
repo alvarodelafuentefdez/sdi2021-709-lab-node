5708a0b1c5d1509dcdf8d396721315fb87330172
uo251950 <uo251950@uniovi.es>
2021-04-18 13:40:23 +0200

    SDI-2021-709-10.2-SW API REST Autenticación.

---
+++ routes/rapicanciones.js
@@ -0,0 +1,70 @@
+module.exports = function (app, gestorBD) {
+    app.get("/api/cancion", function (req, res) {
+        gestorBD.obtenerCanciones({}, function (canciones) {
+            if (canciones == null) {
+                res.status(500);
+                res.json({error: "se ha producido un error"})
+            } else {
+                res.status(200);
+                res.send(JSON.stringify(canciones));
+            }
+        });
+    });
+
+    app.get("/api/cancion/:id", function (req, res) {
+        let criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)}
+        gestorBD.obtenerCanciones(criterio, function (canciones) {
+            if (canciones == null) {
+                res.status(500);
+                res.json({error: "se ha producido un error"})
+            } else {
+                res.status(200);
+                res.send(JSON.stringify(canciones[0]));
+            }
+        });
+    });
+
+    app.delete("/api/cancion/:id", function (req, res) {
+        let criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)}
+        gestorBD.eliminarCancion(criterio, function (canciones) {
+            if (canciones == null) {
+                res.status(500);
+                res.json({error: "se ha producido un error"})
+            } else {
+                res.status(200);
+                res.send(JSON.stringify(canciones));
+            }
+        });
+    });
+
+    app.post("/api/cancion", function (req, res) {
+        let cancion = {nombre: req.body.nombre, genero: req.body.genero, precio: req.body.precio,}
+
+        // ¿Validar nombre, genero, precio?
+
+        gestorBD.insertarCancion(cancion, function (id) {
+            if (id == null) {
+                res.status(500);
+                res.json({error: "se ha producido un error"})
+            } else {
+                res.status(201);
+                res.json({mensaje: "canción insertada", _id: id})
+            }
+        });
+    });
+
+    app.post("/api/autenticar", function (req, res) {
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave')).update(req.body.password).digest('hex');
+        let criterio = {email: req.body.email, password: seguro}
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+                res.status(401);
+                res.json({autenticado: false})
+            } else {
+                let token = app.get('jwt').sign({usuario: criterio.email, tiempo: Date.now() / 1000}, "secreto");
+                res.status(200);
+                res.json({autenticado: true, token: token});
+            }
+        });
+    });
+}
